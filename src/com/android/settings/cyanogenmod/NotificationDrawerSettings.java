e8f5d388e274db890f5d7dde75846272a3492d3f
KangDroid <hyunwoo286@gmail.com>
2015-12-20 01:31:49 +0900

    Revert Time contextual headers
    Change-Id: Icecbfca5a1d1a9ab6b8c73aa32470c543a9ca004

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -33,14 +33,10 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class NotificationDrawerSettings extends SettingsPreferenceFragment  implements Preference.OnPreferenceChangeListener{
+public class NotificationDrawerSettings extends SettingsPreferenceFragment  {
     private static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
-private static final String PREF_CUSTOM_HEADER = "status_bar_custom_header";
-    private static final String PREF_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
 
     private SwitchPreference mForceExpanded;
-    private SwitchPreference mCustomHeader;	
-    private ListPreference mCustomHeaderDefault;
     
     @Override
     public void onCreate(Bundle icicle) {
@@ -51,20 +47,6 @@ private static final String PREF_CUSTOM_HEADER = "status_bar_custom_header";
 
 	mForceExpanded = (SwitchPreference) findPreference(FORCE_EXPANDED_NOTIFICATIONS);
         mForceExpanded.setChecked((Settings.System.getInt(resolver, Settings.System.FORCE_EXPANDED_NOTIFICATIONS, 0) == 1));
-
- 
-        // Status bar custom header
-        mCustomHeader = (SwitchPreference) prefSet.findPreference(PREF_CUSTOM_HEADER);
-        mCustomHeader.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1));
-        mCustomHeader.setOnPreferenceChangeListener(this);
-
-         // Status bar custom header hd
-        mCustomHeaderDefault = (ListPreference) findPreference(PREF_CUSTOM_HEADER_DEFAULT);
-        mCustomHeaderDefault.setOnPreferenceChangeListener(this);
-           int customHeaderDefault = Settings.System.getInt(getContentResolver(),
-                Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, 0);
-        mCustomHeaderDefault.setValue(String.valueOf(customHeaderDefault));
     }
 
     @Override
@@ -77,31 +59,6 @@ private static final String PREF_CUSTOM_HEADER = "status_bar_custom_header";
         super.onResume();
     }
 
-	@Override
-	public boolean onPreferenceChange(Preference preference, Object newValue) {
-	ContentResolver resolver = getActivity().getContentResolver();
-	 if (preference == mCustomHeader) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER,
-                    (Boolean) newValue ? 1 : 0);
-            return true;
-        } else if (preference == mCustomHeaderDefault) {
-           int customHeaderDefault = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(getContentResolver(), 
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT,
-                    customHeaderDefault, UserHandle.USER_CURRENT);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER,
-                    0);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER,
-                    1);
-            return true;
-         }
-         return false;
-	}
-
-
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if  (preference == mForceExpanded) {
@@ -112,7 +69,4 @@ private static final String PREF_CUSTOM_HEADER = "status_bar_custom_header";
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
-
-
 }
-
--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -249,4 +249,3 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                 }
             };
 }
-
