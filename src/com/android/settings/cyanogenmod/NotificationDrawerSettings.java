9c835f6d9ccb5ebb285c0e5557cfd5a5b4c6d6d4
KangDroid <hyunwoo286@gmail.com>
2016-01-03 00:54:35 +0900

    Revert "Re-DO Custom Time StatusBar Header"
    This reverts commit 3bef4060dea94a0cdb4dc034a8aaeca3bfe4522c.

--- src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawerSettings.java
@@ -16,57 +16,28 @@
 package com.android.settings.cyanogenmod;
 
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnCancelListener;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import android.telephony.TelephonyManager;
-import android.text.format.DateFormat;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 import com.android.internal.logging.MetricsLogger;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.search.Indexable;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import cyanogenmod.providers.CMSettings;
-
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class NotificationDrawerSettings extends SettingsPreferenceFragment  {
     private static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
-	private static final String PREF_CUSTOM_HEADER = "status_bar_custom_header";
-    private static final String PREF_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
 
     private SwitchPreference mForceExpanded;
-    private SwitchPreference mCustomHeader;	
-    private ListPreference mCustomHeaderDefault;
     
     @Override
     public void onCreate(Bundle icicle) {
@@ -75,21 +46,8 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  {
         PreferenceScreen prefSet = getPreferenceScreen();
         final ContentResolver resolver = getActivity().getContentResolver();
 
-		mForceExpanded = (SwitchPreference) findPreference(FORCE_EXPANDED_NOTIFICATIONS);
+	mForceExpanded = (SwitchPreference) findPreference(FORCE_EXPANDED_NOTIFICATIONS);
         mForceExpanded.setChecked((Settings.System.getInt(resolver, Settings.System.FORCE_EXPANDED_NOTIFICATIONS, 0) == 1));
-		
-        // Status bar custom header
-        mCustomHeader = (SwitchPreference) prefSet.findPreference(PREF_CUSTOM_HEADER);
-        mCustomHeader.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1));
-        mCustomHeader.setOnPreferenceChangeListener(this);
-
-         // Status bar custom header hd
-        mCustomHeaderDefault = (ListPreference) findPreference(PREF_CUSTOM_HEADER_DEFAULT);
-        mCustomHeaderDefault.setOnPreferenceChangeListener(this);
-           int customHeaderDefault = Settings.System.getInt(getContentResolver(),
-                Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, 0);
-        mCustomHeaderDefault.setValue(String.valueOf(customHeaderDefault));
     }
 
     @Override
@@ -101,30 +59,6 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment  {
     public void onResume() {
         super.onResume();
     }
-	
-	@Override
-	public boolean onPreferenceChange(Preference preference, Object newValue) {
-	ContentResolver resolver = getActivity().getContentResolver();
-	 if (preference == mCustomHeader) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER,
-                    (Boolean) newValue ? 1 : 0);
-            return true;
-        } else if (preference == mCustomHeaderDefault) {
-           int customHeaderDefault = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(getContentResolver(), 
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT,
-                    customHeaderDefault, UserHandle.USER_CURRENT);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER,
-                    0);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.STATUS_BAR_CUSTOM_HEADER,
-                    1);
-            return true;
-         }
-         return false;
-	}
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
