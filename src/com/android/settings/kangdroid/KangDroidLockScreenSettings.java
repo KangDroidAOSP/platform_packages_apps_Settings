ee88e844244780d866321d34c0302111a3e8a58e
Alex Cruz <mazdarider23@gmail.com>
2016-01-03 14:11:20 +0100

    Lockscreen Clock Widget Fonts [2/2]
    Change-Id: I84165a6207fafa6f1e2c9368ea58392c86f3abb3

--- src/com/android/settings/kangdroid/KangDroidLockScreenSettings.java
+++ src/com/android/settings/kangdroid/KangDroidLockScreenSettings.java
@@ -18,12 +18,14 @@ package com.android.settings.kangdroid;
 
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
 import android.provider.Settings;
 import android.provider.SearchIndexableResource;
 
@@ -41,8 +43,10 @@ import java.util.List;
 public class KangDroidLockScreenSettings extends SettingsPreferenceFragment implements Indexable, Preference.OnPreferenceChangeListener {
 	
 	private static final String KEY_LOCKSCREEN_BLUR_RADIUS = "lockscreen_blur_radius";
+	private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
 	
 	private SeekBarPreference mBlurRadius;
+	private ListPreference mLockClockFonts;
 	
 
     @Override
@@ -56,6 +60,12 @@ public class KangDroidLockScreenSettings extends SettingsPreferenceFragment impl
         mBlurRadius.setValue(Settings.System.getInt(resolver,
                 Settings.System.LOCKSCREEN_BLUR_RADIUS, 14));
         mBlurRadius.setOnPreferenceChangeListener(this);
+		
+        mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
+        mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
+                resolver, Settings.System.LOCK_CLOCK_FONTS, 0)));
+        mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+        mLockClockFonts.setOnPreferenceChangeListener(this);
     }
 	
     @Override
@@ -63,19 +73,20 @@ public class KangDroidLockScreenSettings extends SettingsPreferenceFragment impl
         super.onResume();
     }
 	
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-	
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getApplicationContext().getContentResolver();
          if (preference == mBlurRadius) {
             int width = ((Integer)newValue).intValue();
             Settings.System.putInt(resolver,
                     Settings.System.LOCKSCREEN_BLUR_RADIUS, width);
             return true;
-         }
+         } else if (preference == mLockClockFonts) {
+            Settings.System.putInt(resolver, Settings.System.LOCK_CLOCK_FONTS,
+                    Integer.valueOf((String) newValue));
+            mLockClockFonts.setValue(String.valueOf(newValue));
+            mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+            return true;
+        }
         return false;
     }
 	
